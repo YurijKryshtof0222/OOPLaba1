6a8f10f13fbf9a897de9029258a67bfc9813d040
Kryshtof Yurii <yura.krishtof@gmail.com>
2022-09-15 00:30:26 +0300

    Refactoring

--- OOPLaba1/Main.cpp
+++ OOPLaba1/Main.cpp
@@ -21,12 +21,12 @@ int main()
 
 
     //Task #2
-    cout << setw(50) << "Task #2" << endl << setw(40) << "function tabulation" << endl;
+    cout << setw(50) << "Task #2" << endl << setw(55) << "Function tabulation" << endl;
     for (double x = -1.0; x <= 1.0; x += 0.2) {
         B b(x, y, z);
         A a(x, y, z, b.calculate());
         
-        cout << "x = " << to_string(x).substr(0, 4) << setw(20) << "a[x, y, z, b] = " << a.calculate() << setw(20) << "b[x, y, z] = " << b.calculate() << endl;
+        cout << "x = " << to_string(x).substr(0, 4) << setw(20) << "a[x, y, z, b] = " << to_string(a.calculate()).substr(0, 8) << setw(20) << "b[x, y, z] = " << b.calculate() << endl;
     }
     cout << endl;
 
